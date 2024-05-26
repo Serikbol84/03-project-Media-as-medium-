96100255fff4f749130bfb84aa4b822f8f59334a
Serikbol <sh.serikbol@gmail.com>
2024-05-26 23:56:13 +0500

    half job done

--- js/index.js
+++ js/index.js
@@ -13,29 +13,91 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-getTopStories();
-
-const title = document.getElementById('title-1');
-    //   abstract = document.getElementById('p-1')
+const firstTitle = document.getElementById('title-1');
+const secondTitle = document.getElementById('title-2'); 
+const thirdTitle = document.getElementById('title-3'); 
 
 function getTopStories() {
     fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=o31G1KOG06lYvAiTP5ZG6vPACFMtOglA')
     .then(response => response.json())
     .then(data => {
-        const articles = data.results.splice(0, 1);
-        articles.forEach(article => {
-            title.innerHTML += `
-            <div class="text">
-                            <h1 class="title" id="title-1">
-                                <a href="index2.html">${article.title} </a>
-                            </h1>
-                            <p id="p-1">
-                                ${article.abstract}
-                            </p>
-                        </div>`
+        const articles = data.results; 
+        articles.forEach((article, index) => {
+            if (index === 0) {
+                firstTitle.innerHTML += `
+                <div class="text">
+                    <h1 class="title" id="title-1">
+                        <a href="index2.html">${article.title} </a>
+                    </h1>
+                    <p id="p-1">
+                        ${article.abstract}
+                    </p>
+                </div>`
+            } else if (index === 2) {
+                secondTitle.innerHTML += `
+                <div class="text">
+                    <h1 class="title" id="title-2">
+                        <a href="index2.html">${article.title} </a>
+                    </h1>
+                    <p id="p-2">
+                        ${article.abstract}
+                    </p>
+                </div>`
+            } else if (index === 10) {
+                thirdTitle.innerHTML += `
+                <div class="text">
+                    <h1 class="title" id="title-3">
+                        <a href="index2.html">${article.title} </a>
+                    </h1>
+                    <p id="p-3">
+                        ${article.abstract}
+                    </p>
+                </div>`
+            }
+
             console.log(article)
         })
     })
+    .catch(e=>console.log(e))
 
+    //======================================= Еще один вариант (вроде тоже рабочий) ================================================
+    // .then(data => {
+    //     console.log(data)
+    //     const firstArticle = data.results[0]; 
+    //     firstTitle.innerHTML += `
+    //     <div class="text">
+    //         <h1 class="title" id="title-1">
+    //             <a href="index2.html">${firstArticle.title} </a>
+    //         </h1>
+    //         <p id="p-1">
+    //             ${firstArticle.abstract}
+    //         </p>
+    //     </div>`
+
+    //     const secondArticle = data.results[5];
+    //     secondTitle.innerHTML += `
+    //     <div class="text">
+    //         <h1 class="title" id="title-2">
+    //             <a href="index2.html">${secondArticle.title} </a>
+    //         </h1>
+    //         <p id="p-2">
+    //             ${secondArticle.abstract}
+    //         </p>
+    //     </div>`
+
+    //     const thirdArticle = data.results[7];
+    //     thirdTitle.innerHTML += `
+    //     <div class="text">
+    //         <h1 class="title" id="title-3">
+    //             <a href="index2.html">${thirdArticle.title} </a>
+    //         </h1>
+    //         <p id="p-3">
+    //             ${thirdArticle.abstract}
+    //         </p>
+    //     </div>`
+       
+    // })
+    // .catch(e=>console.log(e))
 }
 
+getTopStories();
